91d24192b4f05575e9647f8eadd23db39b898430
atelier-yuki <atelier.of.timbre.3216ariannol@gmail.com>
2020-12-27 12:08:00 +0900

    category_idのActive_hash及び、アソシエーションバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,14 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name, length: { maximum: 40 }
     validates :discription, length: { maximum: 1000 }
-    validates :category_id
+    validates :category_id, numericality: { other_than: 0 }
     validates :item_state_id
     validates :delivery_charge_id
     validates :address_id
     validates :delivery_day_id
     validates :price, length: { in: 300..9999999 }
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
 end
