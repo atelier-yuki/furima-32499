b1b1c2b0edc5371687c26aed5f94eea15607342a
atelier-yuki <atelier.of.timbre.3216ariannol@gmail.com>
2021-01-02 22:02:28 +0900

    商品詳細表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'items#index'
-
-  resources :items, only: [:index, :new, :create]
-
+  
+  resources :items, only: [:index, :new, :creat, :show]
   
   
 end
